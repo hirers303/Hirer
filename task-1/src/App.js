86fe1c352c9f4cc66710591ecabf9af655af4145
nikhilchopra08 <129540378+nikhilchopra08@users.noreply.github.com>
2024-02-05 14:34:02 +0530

    2

---
+++ task-1/src/App.js
@@ -0,0 +1,28 @@
+import './App.css'; 
+import AppLinks from './componets/AppLinks';
+import FeatureList from './componets/FeatureList';
+import Cardd from './componets/Cards';
+
+function App() {
+  return (
+    <div>
+      {/* <Navbar/> */}
+      {/* <Hero/> */}
+      {/* <AnimatedComponent/> */}
+      {/* <Login/> */}
+      {/* <Job/> */}
+      {/* <Heading/> */}
+      {/* <Cards/> */}
+      {/* <JobRegister/> */}
+      {/* <AnimatedText/> */}
+<FeatureList/>
+      <AppLinks/>
+      <Cardd/>
+      
+      {/* <Cards/> */}
+
+    </div>
+  );
+}
+
+export default App;
