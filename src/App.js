8ce575c6a8d14a3b2d78c27bc8095dcc45638b2c
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2024-02-16 21:44:43 +0530

    button for reference for modal added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AboutUs from './components/AboutUs';
 import Modal from './components/Modal';
 import ContactUs from './components/ContactUs';
 
 const App = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <>
             <AboutUs />
             <ContactUs />
-            <Modal />
+            <Modal
+                isOpen={open}
+                closeModal={() => setOpen(false)}
+            />
         </>
     )
 }
